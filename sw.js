38433520baba54b5c108cf28cdca7b578b87e560
zowzoo11-dotcom <zowzoo11@gmail.com>
2025-09-23 16:14:30 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,16 @@
-const CACHE = "eng-levelup-ghpages-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icon-192.png",
-  "./icon-512.png"
-];
+const CACHE = "eng-levelup-ghpages-v2"; // เปลี่ยนชื่อ cache กันชนของเก่า
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys().then((keys) => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
   );
   self.clients.claim();
 });
-
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((r) =>
